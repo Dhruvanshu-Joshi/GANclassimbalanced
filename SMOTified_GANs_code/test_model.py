928e3542ad99db9dfd06f7fabdb8343750680dce
Shazam213 <mulyesoham@gmail.com>
2025-01-29 23:07:16 +0530

    .,

--- SMOTified_GANs_code/test_model.py
+++ SMOTified_GANs_code/test_model.py
@@ -5,7 +5,7 @@ from sklearn.metrics import f1_score, average_precision_score
 from imblearn.metrics import geometric_mean_score
 from statistics import stdev
 
-class test_model():  # Parent class
+class test_model():        
     def __init__(self, X_train, y_train, X_test, y_test):
         self.X_train = X_train
         self.y_train = y_train
@@ -16,26 +16,26 @@ class test_model():  # Parent class
         model = tf.keras.Sequential([
             tf.keras.layers.Dense(256, activation='relu'),
             tf.keras.layers.Dense(128, activation='relu'),
-            tf.keras.layers.Dense(1, activation='sigmoid')  # Using sigmoid for binary classification
+            tf.keras.layers.Dense(1, activation='sigmoid')  # Use sigmoid for probability output
         ])
     
-        model.compile(optimizer='adam',  # Configures the model for training
-                      loss='binary_crossentropy',  # Changed loss to binary_crossentropy
+        model.compile(optimizer='adam',               
+                      loss='binary_crossentropy',  # Use binary cross-entropy for classification
                       metrics=['accuracy'])
-        
         model.fit(self.X_train, self.y_train, epochs=30, verbose=0)
 
         test_loss, test_accuracy = model.evaluate(self.X_test, self.y_test, verbose=0)
         train_loss, train_accuracy = model.evaluate(self.X_train, self.y_train, verbose=0)
         
-        y_preds = model.predict(self.X_test)
-        y_preds = np.ravel((y_preds > 0.5) * 1)
+        y_probs = model.predict(self.X_test).ravel()  # Get probability scores
+        y_preds = (y_probs > 0.5).astype(int)  # Convert probabilities to binary predictions
         
-        F1_score = f1_score(self.y_test, y_preds, average='micro')
-        AP_score = average_precision_score(self.y_test, y_preds)
-        G_mean = geometric_mean_score(self.y_test, y_preds)
+        F1_score = f1_score(self.y_test, y_preds, average='micro')  
+        AP_score = average_precision_score(self.y_test, y_probs)  # Use probabilities, not labels
+        G_mean = geometric_mean_score(self.y_test, y_preds)  # G-Mean
 
         return test_accuracy, train_accuracy, F1_score, AP_score, G_mean
+
 
 
 def test_model_lists(X_train, y_train, X_test, y_test, no_of_trainings):
